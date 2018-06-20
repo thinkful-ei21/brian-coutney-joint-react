13a5041bbfe63de1dbf80ff6242815b410bb46c6
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-06-20 12:11:26 -0400

    2nd push

---
+++ src/background.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Images from './components/images';
+
+export default class Background extends React.Component {
+    constructor(props){
+        super(props);
+    
+        this.state = {
+
+        }}
+    
+
+    render(){
+        return(
+            <div className="background">
+            
+                <Images/>
+            </div>
+        )
+
+    }
+
+
+}
---
+++ src/components/images.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Images extends React.Component {
+    constructor(props){
+    super(props)
+
+    this.state = {
+
+    }}
+
+    
+    render() { 
+        return ( 
+        <div> 
+            
+            <img src="stuff"/> 
+        </div> )};
+}
